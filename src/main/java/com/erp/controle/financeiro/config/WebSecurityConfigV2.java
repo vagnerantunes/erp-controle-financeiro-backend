7e9a79885fd480c2ed334c8a822ba3c9801ee220
vagnerantunes <vagner.antunes0701@outlook.com>
2024-04-14 11:18:35 -0300

    atualizando projeto

--- src/main/java/com/erp/controle/financeiro/config/WebSecurityConfigV2.java
+++ src/main/java/com/erp/controle/financeiro/config/WebSecurityConfigV2.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfigV2 {
-    
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
